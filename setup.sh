e1fd6c7dd9f852bd81f05daccff7acb9cab0ba71
qkieu <quang.p.kieu@gmail.com>
2023-12-14 20:34:58 -0500

    Update setup.sh
    escape "$" character

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ sudo apt install -y ros-humble-desktop python3-rosdep python3-argcomplete python
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
-echo "export ROS_MASTER_URI=http://$(hostname -I | cut -d' ' -f1):11311" >> ~/.bashrc
+echo "export ROS_MASTER_URI=http://\$(hostname -I | cut -d' ' -f1):11311" >> ~/.bashrc
 source ~/.bashrc
 
 # Set up Teensy and Arduino USB rules
